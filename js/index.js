c8e291358a5057f9d0c6e1795df65a3a745b1f22
Cameron-Burkholder <cburkholder19@gmail.com>
2020-09-13 19:38:45 -0400

    09-13-2020 19:38 - Add navigation and mobile styles.

--- js/index.js
+++ js/index.js
@@ -1,23 +1,46 @@
 document.addEventListener("DOMContentLoaded", function() {
   const clients = document.querySelectorAll("div.client");
   const overlay = document.querySelector("div#portfolio-overlay");
-  overlay.style.display = "none";
+  if (overlay) {
+    overlay.style.display = "none";
+  }
   const body = document.querySelector("body");
-  clients.forEach((client) => {
-    client.addEventListener("click", function() {
-      if (client.classList.contains("client-active")) {
-        client.classList.remove("client-active");
-        overlay.style.display = "none";
-        body.style.height = "auto";
-        body.style.overflowY = "auto";
-      } else {
-        client.classList.add("client-active");
-        overlay.style.display = "block";
-        body.style.height = "100VH";
-        body.style.overflowY = "hidden";
-      }
+  if (clients) {
+    clients.forEach((client) => {
+      client.addEventListener("click", function() {
+        if (client.classList.contains("client-active")) {
+          client.classList.remove("client-active");
+          overlay.style.display = "none";
+          body.style.height = "auto";
+          body.style.overflowY = "auto";
+        } else {
+          client.classList.add("client-active");
+          overlay.style.display = "block";
+          body.style.height = "100VH";
+          body.style.overflowY = "hidden";
+        }
+      });
     });
-  });
+  }
+  const navButton = document.querySelector("img.nav-image");
+  const navBody = document.querySelector("div.nav-body");
+  const navLinks = document.querySelectorAll("a.nav-link");
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function() {
+      if (navBody.classList.contains("nav-active")) {
+        navBody.classList.remove("nav-active");
+      } else {
+        navBody.classList.add("nav-active");
+      }
+    })
+  })
+  navButton.addEventListener("click", function() {
+    if (navBody.classList.contains("nav-active")) {
+      navBody.classList.remove("nav-active");
+    } else {
+      navBody.classList.add("nav-active");
+    }
+  })
 });
 
 window.addEventListener("scroll", function() {
